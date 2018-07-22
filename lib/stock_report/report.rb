5aa712c9d265c0b562e1b77022accf73a598329b
Nate Miller <NKDMiller96@gmail.com>
2018-07-22 00:52:12 -0500

    Add function works with xml

--- lib/stock_report/report.rb
+++ lib/stock_report/report.rb
@@ -20,18 +20,16 @@ class StockReport::Report
     total = 0.0
     list = Nokogiri::XML(File.open("report.xml"))
     stocks = list.xpath("//stock")
-    symbol = stocks.xpath("symbol").text
-    quantity = stocks.xpath("quantity").text.to_i
     stocks.each do |stock|
-      symbol = stocks.xpath("symbol").text
-      quantity = stocks.xpath("quantity").text.to_f
+      symbol = stock.xpath("symbol").text
+      quantity = stock.xpath("quantity").text.to_f
       url = "https://www.nasdaq.com/symbol/#{symbol}"
       doc = Nokogiri::HTML(open(url))
       price = doc.css("div.qwidget-dollar").text
       price.delete!("*")
       price.delete!("$")
       price = (price.to_f * quantity).round(2)
-      puts "#{quantity.to_i} of #{symbol} - $#{price}"
+      puts "#{quantity.to_i} of #{symbol.upcase} - $#{price}"
       total += price
     end
     puts "For a total portfolio value of $#{total}"
@@ -47,9 +45,21 @@ class StockReport::Report
     if price == ""
       puts "Stock not found."
     else
-      open('./report.txt', 'a') { |f|
-        f.puts input
-      }
+      puts "Please enter the number of #{input.upcase} owned."
+      quantity = gets.strip
+      report = Nokogiri::XML(File.open("report.xml"))
+      stocks = report.xpath("//stock")
+      new_stock = Nokogiri::XML::Node.new "stock", report
+      new_stock.add_child("<quantity>#{quantity}</quantity>")
+      new_stock.add_child("<symbol>#{input}</symbol>")
+      if stocks.empty?
+        report.xpath("//stocks").each do |node|
+          node << new_stock
+        end
+      else
+        report.xpath("//stock").after(new_stock)
+      end
+      File.write("report.xml", report.to_xml)
     end
   end
 end
