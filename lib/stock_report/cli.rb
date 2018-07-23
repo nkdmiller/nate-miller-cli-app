ebdb52b33288336f0dd820a9b2352f2d537e0faa
Nate Miller <NKDMiller96@gmail.com>
2018-07-23 11:39:56 -0500

    Updated CLI

--- lib/stock_report/cli.rb
+++ lib/stock_report/cli.rb
@@ -9,25 +9,26 @@ class StockReport::CLI
       puts "------------------------------------------"
       puts "Welcome to stock report! What can I do for you?"
       puts "------------------------------------------"
-      puts "Enter 1 to check the price of a certain stock."
-      puts "Enter 2 to list the prices of your stocks."
-      puts "Enter 3 to add a new stock to your list."
-      puts "Enter 4 to remove a stock from your list."
-      puts "Enter 5 to erase your entire list."
+      puts "Enter 1 to get a list of the most active stocks by share volume."
+      puts "Enter 2 to check the price of a certain stock."
+      puts "Enter 3 to list the prices of your stocks."
+      puts "Enter 4 to add a new stock to your list."
+      puts "Enter 5 to remove a stock from your list."
+      puts "Enter 6 to erase your entire list."
       puts "Enter exit to leave the program."
       input = gets.strip
       if input == "1"
-        StockReport::Reporter.new.check
-      elsif input == "2"
-        StockReport::Reporter.new.list
-      elsif input == "3"
-        StockReport::Reporter.new.add
-      elsif input == "4"
-        StockReport::Reporter.new.remove
-      elsif input == "5"
-        StockReport::Reporter.new.clear
-      elsif input == "6"
         StockReport::Reporter.new.most_active
+      elsif input == "2"
+        StockReport::Reporter.new.check
+      elsif input == "3"
+        StockReport::Reporter.new.list
+      elsif input == "4"
+        StockReport::Reporter.new.add
+      elsif input == "5"
+        StockReport::Reporter.new.remove
+      elsif input == "6"
+        StockReport::Reporter.new.clear
       end
     end
   end
